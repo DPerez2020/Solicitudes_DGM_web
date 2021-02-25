8a69aa82be33a1b7e684a6348bc1371667714508
Dawin Rafael Perez Batista <dperez@pixnabilab.com>
2021-02-25 12:55:57 -0400

    se crearon las rutas del login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { NotFoundComponent } from './core/not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth', 
+    loadChildren: () => import('./Auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path:"**",
+    component:NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
